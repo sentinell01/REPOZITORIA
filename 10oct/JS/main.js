a22878f0df2274f2bfbe78f310d42fec4ad121e0
student <student@BTU.local>
2019-10-13 14:45:38 +0400

    init

---
+++ 10oct/JS/main.js
@@ -0,0 +1,31 @@
+let message = "Hello World"
+
+
+const welcomeMessage = "Hello Javascript"
+
+
+
+console.log(message);
+console.log(message);
+
+let year = 2019;
+
+let YEAR = "2020";
+
+// console.log(message);
+// console.log(message);
+// console.log(message);
+
+
+console.log(typeof year);
+console.log(typeof YEAR);
+
+
+let str_1 = "string 1";
+let str_2 = 'string 2';
+let str_3 = `string 3`;
+
+
+console.log( "Welcome " + str_1 );
+console.log( "welcome " + str_2);
+console.log(  `$(str_3)`);
---
+++ 11oct/JS/main.js
@@ -0,0 +1,59 @@
+// let age = 24;
+// let newAge = 30;
+
+// // console.log (age);
+
+// // age = "24";
+// // console.log(age)
+
+// let checked = false; // true
+
+// console.log(checked, typeof checked)
+
+// //
+
+// // console.log( newAge > age )
+// // console.log( newAge < age )
+
+// /**
+//  * 
+//  * user = {name : "", age: 24}
+//  */
+// // const user = null;
+// // console.log(user)
+
+// // falsy values
+
+
+// // "",0, undefined, null, nan
+
+// console.log(Number("2019"))
+// console.log(String(2019))
+// console.log(Boolean(""))
+
+// // alert("hello")
+// // let userName = prompt("შენი სახელი:");
+// // alert(`შენი სახელია - ${userName}`)
+
+
+
+// let sure = confirm("ნამდვილად გსურთ წაშლა?");
+// console.log(sure);
+
+
+// // console.log( 7 % 2)
+// // console.log( 7 ** 2 )
+// // console.log( )
+
+// let counter = 3;
+// // console.log(counter++)
+// // console.log(counter)
+// console.log(++counter)
+// counter = counter + 30;
+
+let A = prompt("რიცხი დაასახელე ბრატ!");
+let B = prompt("არ ვარგა ბრატ ახლიდან");
+
+
+console.log(Number(A) + Number(B));
+
